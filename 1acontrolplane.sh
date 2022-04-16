0946d5231ddd69099026d74bcc2d48419dd12a54
Timi Adewumi <nucleuscaesar@yahoo.com>
2022-04-16 18:59:04 +0100

    Added Files

---
+++ 1acontrolplane.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+sudo hostnamectl set-hostname k8s-control
+
+sudo cat <<EOF >> /etc/hosts
+IP1 k8s-control
+IP2 k8s-worker1
+IP3 k8s-worker2
+EOF
+
+exit
+
+
+
---
+++ 1cnode2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo hostnamectl set-hostname k8s-worker2
+
+sudo cat <<EOF >> /etc/hosts
+IP1 k8s-control
+IP2 k8s-worker1
+IP3 k8s-worker2
+EOF
+
+exit
